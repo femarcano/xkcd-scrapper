fea849b63c8010e5b4e7235f9ad574188fe11cdf
femarcano <santrox@gmail.com>
2023-02-04 18:38:02 +0900

    Correct linter

--- index.js
+++ index.js
@@ -9,9 +9,8 @@ let id;
 for (id = INITIAL_ID_XKCD_COMICS; id < MAX_ID_XKCD_COMICS; id++) {
     const url = `https://xkcd.com/${id}/info.0.json`;
     const {data} = await axios.get(url);
-    const {num, news, transcript, ...restOfComic} = data
-    const comicToStore = {id, ...restOfComic}
-    await fs.writeJson(`./comics/${id}.json`, comicToStore, {spaces: 2})
+    const {num, news, transcript, ...restOfComic} = data;
+    const comicToStore = {id, ...restOfComic};
+    await fs.writeJson(`./comics/${id}.json`, comicToStore, {spaces: 2});
     console.log(data);
-
 }
